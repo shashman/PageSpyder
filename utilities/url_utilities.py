61b84e4d6dbb690f670bd344306b84784b3a8e94
Shashank Maniar <shash@gmx.com>
2020-05-17 02:55:58 +0530

    First version

---
+++ utilities/url_utilities.py
@@ -0,0 +1,12 @@
+def load_urls_from_file(file_path: str):
+    #TODO: Add the code needed to read the text file with URLS in it
+    pass
+
+def load_page(url: str):
+    #TODO: Add the code that reads the URL
+    pass
+
+def scrape_page(page_contents: str):
+    #TODO: Analyze the text
+    pass
+
